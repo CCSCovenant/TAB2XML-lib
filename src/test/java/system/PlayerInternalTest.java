926aa80ab37d5f119e633b060509a866e2e25ad6
Maaz Siddiqui <msiddiqi@my.yorku.ca>
2022-03-09 17:03:29 -0500

    PlayerInternalTest update

--- src/test/java/system/PlayerInternalTest.java
+++ src/test/java/system/PlayerInternalTest.java
@@ -2,13 +2,20 @@ package system;
 
 import converter.Score;
 import custom_exceptions.TXMLException;
-import models.measure.note.Instrument;
-import models.measure.note.Note;
-import models.measure.note.Pitch;
-import models.measure.note.Unpitched;
+import models.measure.Measure;
+import models.measure.note.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import player.MXLPlayer;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class PlayerInternalTest {
 	@Test
@@ -16,21 +23,25 @@ public class PlayerInternalTest {
 		MXLPlayer player = new MXLPlayer(new Score(""));
 
 		Note note = new Note();
-		Assertions.assertEquals(player.getNoteDuration(note),'q');
+		Assertions.assertEquals('q', player.getNoteDuration(note));
 		note.setType("whole");
-		Assertions.assertEquals(player.getNoteDuration(note),'w');
+		Assertions.assertEquals('w', player.getNoteDuration(note));
 		note.setType("half");
-		Assertions.assertEquals(player.getNoteDuration(note),'h');
+		Assertions.assertEquals('h', player.getNoteDuration(note));
 		note.setType("quarter");
-		Assertions.assertEquals(player.getNoteDuration(note),'q');
+		Assertions.assertEquals('q', player.getNoteDuration(note));
+		note.setType("eighth");
+		Assertions.assertEquals('i', player.getNoteDuration(note));
+		note.setType("16th");
+		Assertions.assertEquals('s', player.getNoteDuration(note));
 	}
 	@Test
 	void InstrumentConverterTest() throws TXMLException {
 		MXLPlayer player = new MXLPlayer(new Score(""));
 
-		Assertions.assertEquals(player.getInstrument("default"),"GUNSHOT");
-		Assertions.assertEquals(player.getInstrument("P1-I45"),"PEDAL_HI_HAT");
-		Assertions.assertEquals(player.getInstrument("P1-I36"),"BASS_DRUM");
+		Assertions.assertEquals("GUNSHOT", player.getInstrument("default"));
+		Assertions.assertEquals("PEDAL_HI_HAT", player.getInstrument("P1-I45"));
+		Assertions.assertEquals("BASS_DRUM", player.getInstrument("P1-I36"));
 
 	}
 	@Test
@@ -38,20 +49,20 @@ public class PlayerInternalTest {
 		MXLPlayer player = new MXLPlayer(new Score(""));
 		Note note = new Note();
 		note.setUnpitched(new Unpitched("C",3));
-		Assertions.assertEquals(player.getNoteDetails(note),"V9 I25 ");
+		Assertions.assertEquals("V9 I25 ", player.getNoteDetails(note));
 		note.setUnpitched(new Unpitched("E",3));
-		Assertions.assertEquals(player.getNoteDetails(note),"V9 I25 ");
+		Assertions.assertEquals("V9 I25 ", player.getNoteDetails(note));
 
 	}
 	@Test
-	void NoteDetailpitched() throws TXMLException {
+	void NoteDetailPitched() throws TXMLException {
 		MXLPlayer player = new MXLPlayer(new Score(""));
 		Note note = new Note();
 		note.setPitch(new Pitch("C",1,3));
 		note.setVoice(5);
-		Assertions.assertEquals(player.getNoteDetails(note),"V5 I25 ");
+		Assertions.assertEquals("V5 I25 ", player.getNoteDetails(note));
 		note.setVoice(7);
-		Assertions.assertEquals(player.getNoteDetails(note),"V7 I25 ");
+		Assertions.assertEquals("V7 I25 ", player.getNoteDetails(note));
 	}
 	@Test
 	void NoteDetailInstrument() throws TXMLException{
@@ -59,8 +70,49 @@ public class PlayerInternalTest {
 		Note note = new Note();
 		note.setUnpitched(new Unpitched("C",3));
 		note.setInstrument(new Instrument("P1-I52"));
-		Assertions.assertEquals(player.getNoteDetails(note),"V9 [RIDE_CYMBAL_1] ");
+		Assertions.assertEquals("V9 [RIDE_CYMBAL_1] ", player.getNoteDetails(note));
 		note.setInstrument(null);
-		Assertions.assertEquals(player.getNoteDetails(note),"V9 I25 ");
+		Assertions.assertEquals("V9 I25 ", player.getNoteDetails(note));
+	}
+	@Test
+	void getDotsTest() throws TXMLException {
+		MXLPlayer player = new MXLPlayer(new Score(""));
+		Note note = new Note();
+		List<Dot> dots = new ArrayList<Dot>();
+		note.setUnpitched(new Unpitched("C",3));
+		Assertions.assertEquals("", player.getDots(note));
+		dots.add(new Dot());
+		note.setDots(dots);
+		Assertions.assertEquals(".", player.getDots(note));
+	}
+
+	@Test
+	void getStringTest() throws IOException, TXMLException, URISyntaxException {
+		URL outDirURL = this.getClass().getClassLoader().getResource("../../resources/test/outputs/");
+		Path outDirPath = Path.of(outDirURL.toURI());
+		File outDir= outDirPath.toFile();
+		File[] outputFiles = outDir.listFiles();
+		for (File file : outputFiles) file.delete();
+
+		URL inputDirURL = this.getClass().getClassLoader().getResource("../../resources/test/system/");
+		Path inputDirPath = Path.of(inputDirURL.toURI());
+		File inputDir = inputDirPath.toFile();
+		File[] inputFiles = inputDir.listFiles((dir, name) -> name.toLowerCase().endsWith(".txt"));
+
+		for (File input : inputFiles) {
+			String inputText = Files.readString(input.toPath());
+			StringBuilder inputEdit = new StringBuilder(inputText);
+			for (int i=0;i<inputEdit.length();i++){
+				if (inputEdit.charAt(i)=='\r'){
+					inputEdit.deleteCharAt(i);
+				}
+			}
+
+			Score score = new Score(inputEdit.toString());
+			MXLPlayer player = new MXLPlayer(score);
+			String musicString = player.getString(-1,-1,-1);
+			Assertions.assertEquals("V1 I25 A3h V1 I25 A3h V1 I25 D4q+A3q V1 I25 C4q V1 I25 F4i V1 I25 C4i V1 I25 A3q ", musicString);
+			break;
+		}
 	}
 }
